7bf48ff530e532a3259b3098107360622681fad1
Carlo Nicora <carlo.nicora@gmail.com>
2023-02-07 16:12:38 +0100

    Initial commit

---
+++ src/Enums/Endpoints.php
@@ -0,0 +1,10 @@
+<?php
+namespace Only35\Minimalism\Library\Dictionary;
+
+enum Endpoints: string
+{
+    case User='users';
+    case Roll='rolls';
+    case Stock='stocks';
+    case Menufacturer='manufacturers';
+}
