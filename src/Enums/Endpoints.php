87d5c870a7731138788646dfe1346c2527f9c85f
Carlo Nicora <carlo.nicora@gmail.com>
2023-02-07 18:22:41 +0100

    feat: update endpoints

--- src/Enums/Endpoints.php
+++ src/Enums/Endpoints.php
@@ -3,8 +3,39 @@ namespace Only35\Minimalism\Library\Dictionary\Enums;
 
 enum Endpoints: string
 {
-    case User='users';
-    case Roll='rolls';
-    case Stock='stocks';
-    case Menufacturer='manufacturers';
+    case User='user';
+    case Roll='roll';
+    case Stock='stock';
+    case Manufacturer='manufacturer';
+    case Photograph='photograph';
+
+    /**
+     * @return string
+     */
+    public function getIdKey(
+    ): string
+    {
+        return match ($this) {
+            self::User => 'userId',
+            self::Stock => 'stockId',
+            self::Roll => 'rollId',
+            self::Manufacturer => 'manufacturerId',
+            self::Photograph => 'photographId',
+        };
+    }
+
+    /**
+     * @return string
+     */
+    public function getEndpoint(
+    ): string
+    {
+        return match ($this) {
+            self::User => 'users',
+            self::Stock => 'stocks',
+            self::Roll => 'rolls',
+            self::Manufacturer => 'manufacturers',
+            self::Photograph => 'photographs',
+        };
+    }
 }
